1223b7e98de035fdb8b496362613072197e271e2
Gustavo Neitzke <gutoneitzke12345@gmail.com>
2022-06-26 21:43:02 -0300

    our routes and controllers

--- crud-clientes/routes/web.php
+++ crud-clientes/routes/web.php
@@ -21,4 +21,44 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::middleware(['auth'])->group(function(){
+
+    Route::prefix('paises')->name('paises')->group(function (){
+        Route::get('/', [FuncionariosController::class,'index'])->name('index');
+        Route::get('/create', [FuncionariosController::class,'create'])->name('create');
+        Route::post('/', [FuncionariosController::class,'store'])->name('store');
+        Route::get('/{id}/edit', [FuncionariosController::class, 'edit'])->where('id', '[0-9]+')->name('edit');
+        Route::put('/update/{id}', [FuncionariosController::class, 'update'])->where('id', '[0-9]+')->name('update');
+        Route::delete('/{id}', [FuncionariosController::class,'destroy'])->where('id', '[0-9]+')->name('destroy');
+    });
+
+    Route::prefix('estados')->name('estados')->group(function (){
+        Route::get('/', [FuncionariosController::class,'index'])->name('index');
+        Route::get('/create', [FuncionariosController::class,'create'])->name('create');
+        Route::post('/', [FuncionariosController::class,'store'])->name('store');
+        Route::get('/{id}/edit', [FuncionariosController::class, 'edit'])->where('id', '[0-9]+')->name('edit');
+        Route::put('/update/{id}', [FuncionariosController::class, 'update'])->where('id', '[0-9]+')->name('update');
+        Route::delete('/{id}', [FuncionariosController::class,'destroy'])->where('id', '[0-9]+')->name('destroy');
+    });
+
+    Route::prefix('cidades')->name('cidades')->group(function (){
+        Route::get('/', [FuncionariosController::class,'index'])->name('index');
+        Route::get('/create', [FuncionariosController::class,'create'])->name('create');
+        Route::post('/', [FuncionariosController::class,'store'])->name('store');
+        Route::get('/{id}/edit', [FuncionariosController::class, 'edit'])->where('id', '[0-9]+')->name('edit');
+        Route::put('/update/{id}', [FuncionariosController::class, 'update'])->where('id', '[0-9]+')->name('update');
+        Route::delete('/{id}', [FuncionariosController::class,'destroy'])->where('id', '[0-9]+')->name('destroy');
+    });
+
+    Route::prefix('clientes')->name('clientes')->group(function (){
+        Route::get('/', [FuncionariosController::class,'index'])->name('index');
+        Route::get('/create', [FuncionariosController::class,'create'])->name('create');
+        Route::post('/', [FuncionariosController::class,'store'])->name('store');
+        Route::get('/{id}/edit', [FuncionariosController::class, 'edit'])->where('id', '[0-9]+')->name('edit');
+        Route::put('/update/{id}', [FuncionariosController::class, 'update'])->where('id', '[0-9]+')->name('update');
+        Route::delete('/{id}', [FuncionariosController::class,'destroy'])->where('id', '[0-9]+')->name('destroy');
+    });
+});
+
+
 require __DIR__.'/auth.php';
