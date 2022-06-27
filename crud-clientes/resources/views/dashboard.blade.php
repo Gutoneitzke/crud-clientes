5c35c65e2708f21b795499eeedebabb49cd6bea4
Gustavo Neitzke <gutoneitzke12345@gmail.com>
2022-06-27 13:57:23 -0300

    dashboard with links to pages on cards

--- crud-clientes/resources/views/dashboard.blade.php
+++ crud-clientes/resources/views/dashboard.blade.php
@@ -9,14 +9,20 @@
         <div class="col-sm-3">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200 text-center">
-                    <strong class="text-primary">{{ $paises }}</strong> <b>Países</b>
+                    <strong class="text-primary">{{ $paises }}</strong> 
+                    <x-nav-link :href="route('paises.index')" style="text-decoration: none; font-weight: bold; color: #000;">
+                        Países
+                    </x-nav-link>
                 </div>
             </div>
         </div>
         <div class="col-sm-3">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200 text-center">
-                    <strong class="text-primary">{{ $estados }}</strong> <b>Estados</b>
+                    <strong class="text-primary">{{ $estados }}</strong> 
+                    <x-nav-link :href="route('estados.index')" style="text-decoration: none; font-weight: bold; color: #000;">
+                        Estados
+                    </x-nav-link>
                 </div>
             </div>
         </div>
@@ -24,14 +30,20 @@
         <div class="col-sm-3">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200 text-center">
-                    <strong class="text-primary">{{ $cidades }}</strong> <b>Cidades</b>
+                    <strong class="text-primary">{{ $cidades }}</strong> 
+                    <x-nav-link :href="route('cidades.index')" style="text-decoration: none; font-weight: bold; color: #000;">
+                        Cidades
+                    </x-nav-link>
                 </div>
             </div>
         </div>
         <div class="col-sm-3">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200 text-center">
-                    <strong class="text-primary">{{ $clientes }}</strong> <b>Clientes</b>
+                    <strong class="text-primary">{{ $clientes }}</strong>
+                    <x-nav-link :href="route('clientes.index')" style="text-decoration: none; font-weight: bold; color: #000;">
+                        Clientes
+                    </x-nav-link>
                 </div>
             </div>
         </div>
