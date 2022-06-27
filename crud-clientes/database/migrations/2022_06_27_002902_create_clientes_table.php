9d214c70d46ad2c1557c64f87ef2827e838f3711
Gustavo Neitzke <gutoneitzke12345@gmail.com>
2022-06-26 21:31:55 -0300

    our migrations

---
+++ crud-clientes/database/migrations/2022_06_27_002902_create_clientes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 110);
+            $table->string('email', 10);
+            $table->string('telefone', 11);
+            $table->enum('sexo', ['m', 'f', 'o']);
+            $table->unsignedBigInteger('cidade_id');
+            $table->foreign('cidade_id')->references('id')->on('cidades');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+};
